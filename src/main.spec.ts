5196d38ef5f0d905001bc743154640be10303826
= <glezgalvan.adrian@gmail.com>
2024-04-17 12:01:57 +0100

    First commit

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,7 @@
 import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
 
-describe("Default test", () => {
+describe("Manhattan Distance", () => {
   it("should work", () => {
-    const result = myFunction()
-
-    expect(result).toBe(true)
+    
   })
 })
